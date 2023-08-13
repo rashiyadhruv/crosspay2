5ace2825b194bf357f2d29ae39a596a0b20b692d
rashiyadhruv <rashiyadhruv@gmail.com>
2023-08-13 11:09:32 +0530

    styleing of exchange

--- pages/exchange.js
+++ pages/exchange.js
@@ -1,3 +1,4 @@
+import styles from "./styles/Exchange.module.sass";
 import React from "react";
 import { useState } from "react";
 import { TailSpin } from "react-loader-spinner";
@@ -30,55 +31,47 @@ const Exchange = () => {
   const [amount, setAmount] = useState();
   const [loading, setLoading] = useState(false);
   return (
-    <div className="flex flex-col p-4 h-full w-full items-center rounded-3xl ">
-      <label className="flex mb-4 font-medium text-2xl items-center justify-center color-[#fff]">
-        Transfer
-      </label>
-      <h3 className="mb-2 text-[#50d71e] ">
-        {/* Balance: {accBalance ? `${accBalance} ARV` : "Click Connect button"} */}
-        Balance: {"Click Connect button"}
-      </h3>
+    <div className={styles.wrap}>
+      <div className={styles.card}>
+        <label className={styles.title}>Transfer</label>
 
-      <input
-        type="text"
-        className="bg-[#fff] border border-gray-900 text-secondary-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500
-          block p-2.5 mb-4 min-w-[300px]
-          "
-        placeholder="Recipient address"
-        onChange={(e) => setAddress(e.target.value)}
-      />
-
-      <input
-        type="number"
-        className="bg-[#fff] border border-gray-900 text-secondary-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
-          p-2.5 mb-4 min-w-[300px]
-          "
-        placeholder="Amount"
-        onChange={(e) => setAmount(e.target.value)}
-      />
-
-      {loading ? (
-        <TailSpin
-          height="60"
-          width="60"
-          color=" #ff52d8"
-          ariaLabel="tail-spin-loading"
-          radius="1"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Recipient address"
+          onChange={(e) => setAddress(e.target.value)}
         />
-      ) : (
-        // <TransferBtn title="Send" handleClick={tokenTransfer} />
-        <button
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-white rounded-lg border bg-gray-900 border-gray-900 "
-          onClick={() => {
-            console.log("transfer");
-          }}
-        >
-          <span className="relative px-5 py-2.5 rounded-md ">Transfer</span>
-        </button>
-      )}
+
+        <input
+          type="number"
+          className={styles.input}
+          placeholder="Amount"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+
+        {loading ? (
+          <TailSpin
+            height="60"
+            width="60"
+            color=" #ff52d8"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        ) : (
+          // <TransferBtn title="Send" handleClick={tokenTransfer} />
+          <button
+            className={styles.btn}
+            onClick={() => {
+              console.log("transfer");
+            }}
+          >
+            <span className="relative px-5 py-2.5 rounded-md ">Transfer</span>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
