edfc03cf1e5008dfd817be92d05f31d47040f9de
rashiyadhruv <rashiyadhruv@gmail.com>
2023-08-13 18:48:59 +0530

    pushing dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,12 +2,175 @@ import React from "react";
 import styles from "./styles/Dashboard.module.sass";
 import Layout from "../components/Layout";
 
-const marketplace = () => {
+const Dashboard = () => {
+  const data = [
+    {
+      from: "0x85c...AbB2",
+      to: "0x85c90217E502f59231e7D240022F57CBaE04AbB1",
+      amount: "0.0001",
+      status: "completed",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x167...AbB2",
+      to: "0x81c90217E502f59231e7D240022F57CBaE04AbB2",
+      amount: "0.0003",
+      status: "completed",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x167...AbB2",
+      to: "0x85e90217E502f59231e7D240022F57CBaE04AbB2",
+      amount: "0.0003",
+      status: "completed",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x85c...AbB2",
+      to: "0x85c90217E502f59231e7D240022F57CBaE04AbB1",
+      amount: "0.0001",
+      status: "completed",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x85c...AbB2",
+      to: "0x85c90217E502f59231e7D240022F57CBaE04AbB1",
+      amount: "0.0001",
+      status: "completed",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x85c...AbB2",
+      to: "0x85c90217E502f59231e7D240022F57CBaE04AbB1",
+      amount: "0.0001",
+      status: "completed",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x167...AbB2",
+      to: "0x25c90217E502f59231e7D240022F57CBaE04AbB2",
+      amount: "0.0003",
+      status: "pending",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "receive",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x164...AbB2",
+      to: "0x45c90217E502f59231e7D240022F57CBaE04AbB2",
+      amount: "0.0003",
+      status: "pending",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "receive",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x164...AbB2",
+      to: "0x35c90217E502f59231e7D240022F57CBaE04AbB2",
+      amount: "0.0023",
+      status: "pending",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+    {
+      from: "0x164...AbB2",
+      to: "0x15c90217E502f59231e7D240022F57CBaE04AbB2",
+      amount: "0.0043",
+      status: "pending",
+      chain_from: "ETH",
+      chain_to: "BSC",
+      type: "send",
+      explorer: "https://etherscan.io/tx/0x1234",
+    },
+  ];
+
   return (
     <Layout>
-      <div className={styles.wrap}>History</div>
+      <div className={styles.wrap}>
+        <div className={styles.card}>
+          <div className={styles.title}>History</div>
+          {data
+            .filter((it) => {
+              if (it.status === "completed") return it;
+            })
+            .map((item, index) => {
+              return (
+                <div className={styles.itemwrap} key={index}>
+                  <div className={styles.item__to}>{item.to}</div>
+                  <div className={styles.item__amount}>{item.amount}</div>
+                  <div
+                    className={styles.item__chain}
+                  >{`${item.chain_from}->${item.chain_to}`}</div>
+                  <a href={item.explorer} className={styles.item__explorer}>
+                    explorer
+                  </a>
+                </div>
+              );
+            })}
+        </div>
+        <div className={styles.card}>
+          <div className={styles.title}>Pending send</div>
+          {data
+            .filter((it) => {
+              if (it.status === "pending" && it.type === "send") return it;
+            })
+            .map((item, index) => {
+              return (
+                <div className={styles.itemwrap} key={index}>
+                  <div className={styles.item__to}>{item.to}</div>
+                  <div className={styles.item__amount}>{item.amount}</div>
+                  <div
+                    className={styles.item__chain}
+                  >{`${item.chain_from}->${item.chain_to}`}</div>
+                  <button className={styles.item__explorer_btn}>Approve</button>
+                </div>
+              );
+            })}
+        </div>
+        <div className={styles.card}>
+          <div className={styles.title}>Pending receive</div>
+          {data
+            .filter((it) => {
+              if (it.status === "pending" && it.type === "receive") return it;
+            })
+            .map((item, index) => {
+              return (
+                <div className={styles.itemwrap} key={index}>
+                  <div className={styles.item__to}>{item.to}</div>
+                  <div className={styles.item__amount}>{item.amount}</div>
+                  <div
+                    className={styles.item__chain}
+                  >{`${item.chain_from}->${item.chain_to}`}</div>
+                  <button className={styles.item__explorer_btn}>Request</button>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </Layout>
   );
 };
 
-export default marketplace;
+export default Dashboard;
--- pages/exchange.js
+++ pages/exchange.js
@@ -2,6 +2,7 @@ import styles from "./styles/Exchange.module.sass";
 import React from "react";
 import { useState } from "react";
 import { TailSpin } from "react-loader-spinner";
+import Layout from "@/components/Layout";
 const Exchange = () => {
   //   const {
   //     tokenTransfer,
@@ -32,62 +33,64 @@ const Exchange = () => {
   const [chain, setChain] = useState("ETH"); // ["BSC", "ETH"
   const [loading, setLoading] = useState(false);
   return (
-    <div className={styles.wrap}>
-      <div className={styles.card}>
-        <label className={styles.title}>Transfer</label>
+    <Layout>
+      <div className={styles.wrap}>
+        <div className={styles.card}>
+          <label className={styles.title}>Transfer</label>
 
-        <input
-          type="text"
-          className={styles.input}
-          placeholder="Recipient address"
-          onChange={(e) => setAddress(e.target.value)}
-        />
-
-        <input
-          type="number"
-          className={styles.input}
-          placeholder="Amount"
-          onChange={(e) => setAmount(e.target.value)}
-        />
-
-        <select
-          className={styles.chain}
-          value={chain}
-          onChange={(e) => setChain(e.target.value)}
-        >
-          <option name="Etherium"> Etherium</option>
-          <option name="Binance">Binance</option>
-          <option name="Tezos">Tezos</option>
-          <option name="Polygon">Polygon</option>
-        </select>
-
-        {loading ? (
-          <TailSpin
-            height="60"
-            width="60"
-            color=" #ff52d8"
-            ariaLabel="tail-spin-loading"
-            radius="1"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Recipient address"
+            onChange={(e) => setAddress(e.target.value)}
           />
-        ) : (
-          // <TransferBtn title="Send" handleClick={tokenTransfer} />
-          <button
-            className={styles.btn}
-            onClick={() => {
-              console.log("transfer");
-              console.log(address);
-              console.log(amount);
-              console.log(chain);
-            }}
+
+          <input
+            type="number"
+            className={styles.input}
+            placeholder="Amount"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+
+          <select
+            className={styles.chain}
+            value={chain}
+            onChange={(e) => setChain(e.target.value)}
           >
-            <span className="relative px-5 py-2.5 rounded-md ">Transfer</span>
-          </button>
-        )}
+            <option name="Etherium"> Etherium</option>
+            <option name="Binance">Binance</option>
+            <option name="Tezos">Tezos</option>
+            <option name="Polygon">Polygon</option>
+          </select>
+
+          {loading ? (
+            <TailSpin
+              height="60"
+              width="60"
+              color=" #ff52d8"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          ) : (
+            // <TransferBtn title="Send" handleClick={tokenTransfer} />
+            <button
+              className={styles.btn}
+              onClick={() => {
+                console.log("transfer");
+                console.log(address);
+                console.log(amount);
+                console.log(chain);
+              }}
+            >
+              <span className="relative px-5 py-2.5 rounded-md ">Transfer</span>
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
